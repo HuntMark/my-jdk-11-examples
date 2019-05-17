89ec7d52f0b9ac7b2f4cfee16e54f67d8e684712
Rinat_Zalyaletdinov <Rinat_Zalyaletdinov@epam.com>
2019-05-17 14:07:40 +0300

    Use the simple generic class in example

---
+++ core-java/volume-I-fundamentals/generic-programming/src/main/java/pair1/PairTest1.java
@@ -0,0 +1,41 @@
+package pair1;
+
+import pair.Pair;
+
+public class PairTest1 {
+    public static void main(String[] args) {
+        String[] words = {"Mary", "had", "a", "little", "lamb"};
+        Pair<String> mm = ArrayAlg.minmax(words);
+        System.out.println("min = " + mm.getFirst());
+        System.out.println("max = " + mm.getSecond());
+    }
+}
+
+class ArrayAlg {
+
+    /**
+     * Gets the minimum and maximum of an array of strings.
+     *
+     * @param a an array of strings
+     * @return a pair with the min and max values, or null if <code>a</code> is null or empty
+     */
+    static Pair<String> minmax(String[] a) {
+        if (a == null || a.length == 0) {
+            return null;
+        }
+
+        String min = a[0];
+        String max = a[0];
+
+        for (String s : a) {
+            if (min.compareTo(s) > 0) {
+                min = s;
+            }
+            if (max.compareTo(s) < 0) {
+                max = s;
+            }
+        }
+
+        return new Pair<>(min, max);
+    }
+}
