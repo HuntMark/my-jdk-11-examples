7f08c6e6ad2e0a4c41dc574105f4eb7c5a0770fa
Rinat_Zalyaletdinov <Rinat_Zalyaletdinov@epam.com>
2019-05-22 15:12:00 +0300

    Use Lock object for synchronization

--- core-java/volume-I-fundamentals/concurrency/src/main/java/threads/Bank.java
+++ core-java/volume-I-fundamentals/concurrency/src/main/java/threads/Bank.java
@@ -1,6 +1,7 @@
 package threads;
 
 import java.util.Arrays;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * A bank with a number of bank accounts.
@@ -8,6 +9,7 @@ import java.util.Arrays;
 public class Bank {
 
     private final double[] accounts;
+    private ReentrantLock bankLock = new ReentrantLock();
 
     /**
      * Constructs the bank.
@@ -31,11 +33,16 @@ public class Bank {
         if (accounts[from] < amount) {
             return;
         }
-        System.out.print(Thread.currentThread());
-        accounts[from] -= amount;
-        System.out.printf(" %10.2f from %d to %d", amount, from, to);
-        accounts[to] += amount;
-        System.out.printf(" Total Balance: %10.2f%n", getTotalBalance());
+        bankLock.lock();
+        try {
+            System.out.print(Thread.currentThread());
+            accounts[from] -= amount;
+            System.out.printf(" %10.2f from %d to %d", amount, from, to);
+            accounts[to] += amount;
+            System.out.printf(" Total Balance: %10.2f%n", getTotalBalance());
+        } finally {
+            bankLock.unlock();
+        }
     }
 
     /**
